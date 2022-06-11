bb4b809e52d783065c0dc6cebeaa6c15198069a8
Bala Gangadhar Reddy,Bhuma <bgrbhuma@gmail.com>
2022-06-11 17:37:56 -0400

    Final Goal Achieved

--- src/com/EmailApplication/EmailApp.java
+++ src/com/EmailApplication/EmailApp.java
@@ -2,7 +2,7 @@ package com.EmailApplication;
 
 public class EmailApp {
     public static void main(String[] args) {
-       EmployeeEmail em1 = new EmployeeEmail("Bala", "Gangadhar");
+        EmployeeEmail em1 = new EmployeeEmail("Bala_Gangadhar_Reddy", "Bhuma");
 
         //Use setters to pass values into the dependency class to change them or supply them if the dependency class does not have those values defined.
         em1.setMailboxCapacity(500);
