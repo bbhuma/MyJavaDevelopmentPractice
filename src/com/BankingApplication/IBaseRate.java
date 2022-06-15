8a61a62998b619d01492cb24fd16334bf5985727
Bala Gangadhar Reddy,Bhuma <bgrbhuma@gmail.com>
2022-06-14 22:40:56 -0400

    Moving Banking application to seperate project

--- src/com/BankingApplication/IBaseRate.java
+++
@@ -1,7 +0,0 @@
-package com.BankingApplication;
-
-public interface IBaseRate {
-    default double getBaseRate() {    //Interface abstract methods can not have a body, but default methods of the interface can have a body.
-        return 2.50;
-    }
-}
